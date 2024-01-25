8a0f36cb6445891d120f84360947912852b8cb20
Alexandr Kenik <kenik000000@gmail.com>
2024-01-25 22:33:47 +0700

    test: add tests for UserService

--- src/main/java/com/example/bank/services/UserService.java
+++ src/main/java/com/example/bank/services/UserService.java
@@ -20,66 +20,62 @@ public class UserService {
     private final PasswordEncoder encoder;
     private final UserRepository userRepository;
     private final PersonalDetailsService personalDetailsService;
-    private final String CONFIRM_CODE=UUID.randomUUID().toString();
     private final PasswordEncoder passwordEncoder;
     private final SessionService sessionService;
+    private final String CONFIRM_CODE = UUID.randomUUID().toString();
 
-    public List<UserEntity> findAll(){
+    public List<UserEntity> findAll() {
         return userRepository.findAll();
     }
 
-    public UserEntity findById(Long id){
-        return userRepository.findById(id).
-                orElseThrow(()->new UsernameNotFoundException(String.format("User with %s id does`t exist",id)));
-    }
-    public UserEntity findByEmail(String email){
-       return userRepository.findByEmail(email).orElseThrow(()->
-                new UsernameNotFoundException(String.format("User with %s email does`t exist",email)));
+    public UserEntity findById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException(String.format("User with %s id does`t exist", id)));
     }
 
-    public boolean existsByEmail(String email){
+    public UserEntity findByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(String.format("User with %s email does`t exist", email)));
+    }
+
+    public boolean existsByEmail(String email) {
         return userRepository.existsByEmail(email);
     }
-    public void createNewUserAfterRegistration(UserEntity userEntity){
-
-        userEntity.setPassword(encoder.encode(userEntity.getPassword()));
-        userEntity.setStatus(Status.ACTIVE);
-        userEntity.setRole(Role.UNCONFIRMED_USER);
-        userEntity.setConfirmationCode(CONFIRM_CODE);
-
-
-        UserEntity savedUser=userRepository.save(userEntity);
-        personalDetailsService.createPersonalDetails(savedUser);
-
-
-    }
-    public void changePassword(String email,String password){
-        UserEntity user=findByEmail(email);
-        user.setConfirmationCode(UUID.randomUUID().toString());
-        user.setPassword(passwordEncoder.encode(password));
-        userRepository.save(user);
-    }
-
 
     public void updateUser(UserEntity updateUser) {
         userRepository.save(updateUser);
     }
 
-    public void deleteUserById(Long id){
+    public void deleteUserById(Long id) {
         userRepository.deleteById(id);
+    }
+
+    public void createNewUserAfterRegistration(UserEntity userEntity) {
+        userEntity.setPassword(encoder.encode(userEntity.getPassword()));
+        userEntity.setStatus(Status.ACTIVE);
+        userEntity.setRole(Role.UNCONFIRMED_USER);
+        userEntity.setConfirmationCode(CONFIRM_CODE);
+        UserEntity savedUser = userRepository.save(userEntity);
+        personalDetailsService.createPersonalDetails(savedUser);
+    }
+
+    public void changePassword(String email, String password) {
+        UserEntity user = findByEmail(email);
+        user.setConfirmationCode(UUID.randomUUID().toString());
+        user.setPassword(passwordEncoder.encode(password));
+        updateUser(user);
     }
 
     public boolean confirmEmail(String email, String confirmationCode) {
 
         UserEntity user = findByEmail(email);
-        String actualConfirmationCode=user.getConfirmationCode();
+        String actualConfirmationCode = user.getConfirmationCode();
 
-        boolean equalsConfirmationCode= Objects.equals(actualConfirmationCode,confirmationCode);
+        boolean equalsConfirmationCode = Objects.equals(actualConfirmationCode, confirmationCode);
 
-        if(equalsConfirmationCode){
+        if (equalsConfirmationCode) {
             user.setRole(Role.USER);
+            user.setConfirmationCode(UUID.randomUUID().toString());
             updateUser(user);
-            sessionService.updateUserRole(user.getEmail(),Role.USER);
+            sessionService.updateUserRole(user.getEmail(), Role.USER);
             return true;
         }
 
--- src/test/java/com/example/bank/services/UserServiceTest.java
+++ src/test/java/com/example/bank/services/UserServiceTest.java
@@ -5,9 +5,11 @@ import com.example.bank.model.Status;
 import com.example.bank.model.UserEntity;
 import com.example.bank.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,108 +21,168 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
     @Mock
     private PasswordEncoder passwordEncoder;
     @Mock
     private UserRepository userRepository;
     @Mock
     private PersonalDetailsService personalDetailsService;
+    @Mock
+    private SessionService sessionService;
+
+    @Spy
     @InjectMocks
     private UserService userService;
 
     @Test
-    public void findAllUsersTest() throws Exception{
+    public void findAllUsersTest() throws Exception {
         userService.findAll();
         verify(userRepository, times(1)).findAll();
     }
 
     @Test
-    public void findUserByIdTest_Positive() throws Exception{
-        var id=1L;
+    public void findUserByIdTest_Positive() throws Exception {
+        var id = 1L;
         doReturn(Optional.of(new UserEntity()))
-                        .when(userRepository)
-                                .findById(id);
-        UserEntity result=userService.findById(id);
+                .when(userRepository)
+                .findById(id);
+        UserEntity result = userService.findById(id);
 
         assertNotNull(result);
         verify(userRepository, times(1)).findById(id);
     }
 
     @Test
-    public void findUserByIdTestNegative_ThrowsUserNotFoundException() throws Exception{
-        var id=1L;
+    public void findUserByIdTestNegative_ThrowsUserNotFoundException() throws Exception {
+        var id = 1L;
         doReturn(Optional.empty())
-                        .when(userRepository)
-                                .findById(id);
-        assertThrows(UsernameNotFoundException.class,()->userService.findById(id));
+                .when(userRepository)
+                .findById(id);
+        assertThrows(UsernameNotFoundException.class, () -> userService.findById(id));
         verify(userRepository, times(1)).findById(id);
     }
 
     @Test
-    public void findUserByEmailTest_Positive() throws Exception{
-        var email="some_email";
+    public void findUserByEmailTest_Positive() throws Exception {
+        var email = "some_email";
         doReturn(Optional.of(new UserEntity()))
-                        .when(userRepository)
-                                .findByEmail(email);
-        UserEntity result=userService.findByEmail(email);
+                .when(userRepository)
+                .findByEmail(email);
+        UserEntity result = userService.findByEmail(email);
 
         assertNotNull(result);
         verify(userRepository, times(1)).findByEmail(email);
     }
 
     @Test
-    public void findUserByEmailTestNegative_ThrowsUserNoFoundException() throws Exception{
-        var email="some_email";
+    public void findUserByEmailTestNegative_ThrowsUserNoFoundException() throws Exception {
+        var email = "some_email";
         doReturn(Optional.empty())
-                        .when(userRepository)
-                                .findByEmail(email);
-        assertThrows(UsernameNotFoundException.class,()->userService.findByEmail(email));
+                .when(userRepository)
+                .findByEmail(email);
+        assertThrows(UsernameNotFoundException.class, () -> userService.findByEmail(email));
         verify(userRepository, times(1)).findByEmail(email);
     }
 
     @Test
-    public void existsUserByEmailTest() throws Exception{
+    public void existsUserByEmailTest() throws Exception {
         assertFalse(userService.existsByEmail("some_email"));
-        verify(userRepository,times(1)).existsByEmail("some_email");
+        verify(userRepository, times(1)).existsByEmail("some_email");
     }
 
     @Test
-    public void UpdateUserTest() throws Exception{
+    public void UpdateUserTest() throws Exception {
         userService.updateUser(new UserEntity());
-        verify(userRepository,times(1)).save(new UserEntity());
+        verify(userRepository, times(1)).save(new UserEntity());
     }
 
     @Test
-    public void DeleteUserByIdTest() throws Exception{
-        var id=1L;
+    public void deleteUserByIdTest() throws Exception {
+        var id = 1L;
         userService.deleteUserById(id);
-        verify(userRepository,times(1)).deleteById(id);
+        verify(userRepository, times(1)).deleteById(id);
     }
 
 
     @Test
-    public void addUserTest() throws Exception{
-        UserEntity user=new UserEntity();
+    public void addUserTest() throws Exception {
+        var user = new UserEntity();
         user.setEmail("some_email");
-        user.setPassword("helll");
+        user.setPassword("helo");
 
         doReturn("encode_password")
-                        .when(passwordEncoder)
-                                .encode(user.getPassword());
+                .when(passwordEncoder)
+                .encode(user.getPassword());
 
         userService.createNewUserAfterRegistration(user);
 
-        assertEquals("some_email",user.getEmail());
+        assertEquals("some_email", user.getEmail());
         assertNotNull(user.getPassword());
-        assertEquals(Status.ACTIVE,user.getStatus());
-        assertEquals(Role.UNCONFIRMED_USER,user.getRole());
+        assertEquals(Status.ACTIVE, user.getStatus());
+        assertEquals(Role.UNCONFIRMED_USER, user.getRole());
         assertNotNull(user.getConfirmationCode());
 
-        verify(userRepository, times(1)).save(user);
+        UserEntity savedUser = verify(userRepository, times(1)).save(user);
+        verify(personalDetailsService, times(1)).createPersonalDetails(savedUser);
+    }
 
-        verify(personalDetailsService, times(1)).createPersonalDetails(user);
+    @Test
+    public void changePasswordTest() throws Exception{
+        var oldPassword="old_password";
+        var oldConfirmationCode="old_confirmation_code";
+        var user=new UserEntity();
+        user.setPassword(oldPassword);
+        user.setConfirmationCode(oldConfirmationCode);
+
+        doReturn(user).when(userService).findByEmail("some_email");
+        doReturn("new_password").when(passwordEncoder).encode("new_password");
+
+        userService.changePassword("some_email","new_password");
+
+        assertNotNull(user.getPassword());
+        assertNotEquals(oldPassword,user.getPassword());
+        assertNotNull(user.getConfirmationCode());
+        assertNotEquals(oldConfirmationCode,user.getConfirmationCode());
+
+        verify(passwordEncoder,times(1)).encode("new_password");
+        verify(userService,times(1)).findByEmail("some_email");
+        verify(userService,times(1)).updateUser(user);
+
     }
 
 
+    @Test
+    public void confirmEmail_Should_Return_True() throws Exception {
+        var oldConfirmationCode="old_code";
+        var newConfirmationCode="new_code";
+        var user=new UserEntity();
+        user.setConfirmationCode(oldConfirmationCode);
+        doReturn(user).when(userService).findByEmail("email");
+        boolean isConfirmed = userService.confirmEmail("email", oldConfirmationCode);
+        assertEquals(Role.USER,user.getRole());
+        assertNotNull( user.getConfirmationCode());
+        assertNotEquals(oldConfirmationCode,user.getConfirmationCode());
+        assertTrue(isConfirmed);
 
+        verify(userService,times(1)).findByEmail("email");
+        verify(userService,times(1)).updateUser(user);
+        verify(sessionService,times(1)).updateUserRole(user.getEmail(), Role.USER);
+    }
+
+    @Test
+    public void confirmEmail_Should_Return_False() throws Exception {
+        var confirmationCodeFromMail="mail_code";
+        var userConfirmationCode="user_code";
+        var user=new UserEntity();
+        user.setConfirmationCode(userConfirmationCode);
+        doReturn(user).when(userService).findByEmail("email");
+        boolean isConfirmed = userService.confirmEmail("email", confirmationCodeFromMail);
+        assertNotEquals(Role.USER,user.getRole());
+        assertFalse(isConfirmed);
+
+        verify(userService,times(1)).findByEmail("email");
+        verify(userService,times(0)).updateUser(user);
+        verify(sessionService,times(0)).updateUserRole(user.getEmail(), Role.USER);
+    }
 }
